30d062525d28db09d1d9a43ae413ae67e91b4c8e
Josh <shijun_z@163.com>
2020-08-30 22:38:51 +0800

    修改圆形图片Editor右键创建方法

--- Assets/Editor/UGUI/SuperImage/SuperImageEditor.cs
+++ Assets/Editor/UGUI/SuperImage/SuperImageEditor.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEditor.Experimental.SceneManagement;
 
 [CustomEditor(typeof(SuperImage), true)]
 public class SuperImageEditor : Editor
@@ -24,21 +27,14 @@ public class SuperImageEditor : Editor
     [MenuItem("GameObject/UI/SuperImage")]
     static void SuperImage()
     {
-        GameObject parent = Selection.activeGameObject;
-        RectTransform parentCanvasRenderer = parent?.GetComponent<RectTransform>();
-        if (parentCanvasRenderer)
-        {
-            GameObject go = new GameObject("SuperImage");
-            go.transform.SetParent(parent.transform, false);
-            go.AddComponent<RectTransform>();
-            go.AddComponent<CanvasRenderer>();
-            go.AddComponent<SuperImage>();
-            Selection.activeGameObject = go;
-        }
-        else
-        {
-            EditorUtility.DisplayDialog("SuperImage", "You must make the SuperImage object as a child of a Canvas.", "Ok");
-        }
+        GameObject parent = GetOrCreateCanvasGameObject();
+
+        GameObject go = new GameObject("SuperImage");
+        go.transform.SetParent(parent.transform, false);
+        go.AddComponent<RectTransform>();
+        go.AddComponent<CanvasRenderer>();
+        go.AddComponent<SuperImage>();
+        Selection.activeGameObject = go;
     }
     // Update is called once per frame
     public override void OnInspectorGUI()
@@ -54,5 +50,94 @@ public class SuperImageEditor : Editor
         EditorGUILayout.PropertyField(segements);
 
         serializedObject.ApplyModifiedProperties();
+    }
+
+    static bool IsValidCanvas(Canvas canvas)
+    {
+        if (canvas == null || !canvas.gameObject.activeInHierarchy)
+            return false;
+
+        // It's important that the non-editable canvas from a prefab scene won't be rejected,
+        // but canvases not visible in the Hierarchy at all do. Don't check for HideAndDontSave.
+        if (EditorUtility.IsPersistent(canvas) || (canvas.hideFlags & HideFlags.HideInHierarchy) != 0)
+            return false;
+
+        if (StageUtility.GetStageHandle(canvas.gameObject) != StageUtility.GetCurrentStageHandle())
+            return false;
+
+        return true;
+    }
+    static GameObject GetOrCreateCanvasGameObject()
+    {
+        GameObject selected = Selection.activeGameObject;
+
+        // Try to find a gameobject that is the selected GO or one if its parents.
+        Canvas canvas = selected?.GetComponentInParent<Canvas>();
+        if (IsValidCanvas(canvas))
+            return selected;
+
+        // No canvas in selection or its parents? Then use any valid canvas.
+        // We have to find all loaded Canvases, not just the ones in main scenes.
+        Canvas[] canvasArray = StageUtility.GetCurrentStageHandle().FindComponentsOfType<Canvas>();
+        for (int i = 0; i < canvasArray.Length; i++)
+            if (IsValidCanvas(canvasArray[i]))
+                return canvasArray[i].gameObject;
+
+        // No canvas in the scene at all? Then create a new one.
+        return CreateNewUI();
+    }
+
+    static GameObject CreateNewUI()
+    {
+        // Root for the UI
+        var root = new GameObject("Canvas");
+        root.layer = LayerMask.NameToLayer("UI");
+        Canvas canvas = root.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        root.AddComponent<CanvasScaler>();
+        root.AddComponent<GraphicRaycaster>();
+
+        // Works for all stages.
+        StageUtility.PlaceGameObjectInCurrentStage(root);
+        bool customScene = false;
+        PrefabStage prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+        if (prefabStage != null)
+        {
+            root.transform.SetParent(prefabStage.prefabContentsRoot.transform, false);
+            customScene = true;
+        }
+
+        Undo.RegisterCreatedObjectUndo(root, "Create " + root.name);
+
+        // If there is no event system add one...
+        // No need to place event system in custom scene as these are temporary anyway.
+        // It can be argued for or against placing it in the user scenes,
+        // but let's not modify scene user is not currently looking at.
+        if (!customScene)
+            CreateEventSystem(false, null);
+        return root;
+    }
+
+    static void CreateEventSystem(bool select, GameObject parent)
+    {
+        StageHandle stage = parent == null ? StageUtility.GetCurrentStageHandle() : StageUtility.GetStageHandle(parent);
+        var esys = stage.FindComponentOfType<EventSystem>();
+        if (esys == null)
+        {
+            var eventSystem = new GameObject("EventSystem");
+            if (parent == null)
+                StageUtility.PlaceGameObjectInCurrentStage(eventSystem);
+            else
+                GameObjectUtility.SetParentAndAlign(eventSystem, parent);
+            esys = eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+
+            Undo.RegisterCreatedObjectUndo(eventSystem, "Create " + eventSystem.name);
+        }
+
+        if (select && esys != null)
+        {
+            Selection.activeGameObject = esys.gameObject;
+        }
     }
 }
