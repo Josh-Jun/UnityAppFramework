7bbfb8b47d90689f0bb289d7e1316faaf03a8c77
Josh-Jun <shijun_z@163.com>
2022-04-19 13:18:58 +0800

    AppConfig修改

--- Assets/Scripts/Framework/Interface/AppConfig.cs
+++ Assets/Scripts/Framework/Interface/AppConfig.cs
@@ -7,18 +7,18 @@ public class AppConfig : ScriptableObject
 {
     [Header("App Config")]
 
-    [Tooltip("App的调试模式")]
-    public bool IsDebug;//是否打印Log
-    [Tooltip("App服务器是否生产服务器")]
-    public bool IsProServer;//是否测试服生产服
+    [Tooltip("App调试模式")]
+    public bool IsDebug;
+    [Tooltip("App服务器是否生产服")]
+    public bool IsProServer;
     [Tooltip("App是否热更")]
-    public bool IsHotfix;//是否热更
+    public bool IsHotfix;
     [Tooltip("App是否加载AB包")]
-    public bool IsLoadAB;//是否加载AB
-    [Tooltip("App是否运行XLua脚本")]
-    public bool RunXLua;//是否热更
-    [Tooltip("App的运行帧频，默认30帧")]
-    public int AppFrameRate = 30;//App的运行帧频
+    public bool IsLoadAB;
+    [Tooltip("App运行XLua脚本")]
+    public bool RunXLua;
+    [Tooltip("App运行帧频，默认30帧")]
+    public int AppFrameRate = 30;
     [Tooltip("App安卓包不同渠道")]
-    public ApkTarget ApkTarget;//App安卓包不同渠道
+    public ApkTarget ApkTarget;
 }
