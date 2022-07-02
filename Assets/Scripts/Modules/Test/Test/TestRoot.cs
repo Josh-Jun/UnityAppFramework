69b3f5d95b5fba772c6e539c25c22b1616400624
大腿Plus <shijun_z@163.com>
2022-07-02 21:09:37 +0800

    Test update

--- Assets/Scripts/Modules/Test/Test/TestRoot.cs
+++ Assets/Scripts/Modules/Test/Test/TestRoot.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using RenderHeads.Media.AVProVideo;
 using TableData;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
@@ -37,37 +38,49 @@ namespace Test
         {
             
         }
-
+        
         public void Test()
         {
+            //音频播放
             string audio_path = "";
             AudioClip audio = AssetsManager.Instance.LoadAsset<AudioClip>(audio_path);
             AudioManager.Instance.PlayBackgroundAudio(audio);//背景音乐，循环播放
             AudioManager.Instance.PlayEffectAudio(audio);//特效音乐，播放一次，可叠加播放
-            
+            //视频播放（unity自带视频播放器）
             string vidio_path = "";
             VideoClip video = AssetsManager.Instance.LoadAsset<VideoClip>(vidio_path);
             VideoManager.Instance.PlayVideo(null,video);//播放视频
             VideoManager.Instance.PlayVideo(null,"视频地址", () => { });//播放视频
-
+            //AVPro视频播放
             AVProManager.Instance.OpenMedia(MediaPathType.AbsolutePathOrURL, "视频地址");
             AVProManager.Instance.AddMediaPlayerEvent(MediaPlayerEvent.EventType.FinishedPlaying,() => { });//播放完成事件
-
+            //获取配置表
             TableManager.Instance.GetTable<TestTableData>("");//获取配置表
-
+            //时间任务
             int timeid1 = -1;
             timeid1 = TimerManager.Instance.StartTimer((time) => { });//一直执行 相当于Update
             TimerManager.Instance.EndTimer(timeid1);
             int timeid2 = -1;
             timeid2 = TimerTaskManager.Instance.AddTimeTask(() => { }, 1f, TimeUnit.Second, 1);//1秒后执行一次
             TimerTaskManager.Instance.DeleteTimeTask(timeid2);
-
+            //Animator动画播放
             Animator animator = window.TryGetComponent<Animator>();//获取脚本组件，没有就自动添加
             animator.Play("动画名", () => { });//播放动画
             animator.PlayBack("动画名", () => { });//倒放动画
-            
+            //序列帧动画
             Image image = window.TryGetComponent<Image>();//获取脚本组件，没有就自动添加
-            RawImage rawimage = window.TryGetComponent<RawImage>();//获取脚本组件，没有就自动添加
+            image.PlayFrames(new List<Sprite>());
+            //gameobject对象和脚本显隐
+            window.SetGameObjectActive();
+            window.SetComponentEnable<TestWindow>(false);
+            //点击事件
+            window.OnClick(() => { });
+            window.gameObject.OnClick(() => { });
+            window.TryGetComponent<Button>().BtnOnClick(() => { });
+            window.gameObject.AddEventTrigger(EventTriggerType.PointerClick, (arg) => { });
+            window.AddEventTrigger(EventTriggerType.PointerClick, (arg) => { });
+            //平台管理类,不同平台对应的原生方法和属性
+            PlatformManager.Instance.GetDataPath("");
 
         }
         private void TakePhoto()
