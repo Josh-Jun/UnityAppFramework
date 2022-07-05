3e390546088fb22368ed9222dbda4778570d3a71
Josh-Jun <shijun_z@163.com>
2022-07-05 17:34:55 +0800

    修改脚本自动生成工具导致导入图片等资源失败的bug

--- Assets/AppFramework/Editor/Tools/AddScriptTemplatesEditor.cs
+++ Assets/AppFramework/Editor/Tools/AddScriptTemplatesEditor.cs
@@ -5,6 +5,7 @@ using System.IO;
 public class AddScriptTemplatesEditor : UnityEditor.AssetModificationProcessor
 {
     private static string[] temps = { "Root", "Window" };
+
     /// <summary>  
     /// 此函数在asset被创建完，文件已经生成到磁盘上，但是没有生成.meta文件和import之前被调用  
     /// </summary>  
@@ -14,16 +15,19 @@ public class AddScriptTemplatesEditor : UnityEditor.AssetModificationProcessor
         var newFilePath = newFileMeta.Replace(".meta", "");
         var realPath = Application.dataPath.Replace("Assets", "") + newFilePath;
         var scriptContent = File.ReadAllText(realPath);
-
-        //这里实现自定义的一些规则
-        //scriptContent = scriptContent.Replace("#SCRIPTNAME#", Path.GetFileNameWithoutExtension(newFilePath));
-        var namespaces = Path.GetFileNameWithoutExtension(newFilePath);
-        for (int i = 0; i < temps.Length; i++)
+        if (Path.GetExtension(newFilePath) == ".txt" || Path.GetExtension(newFilePath) == ".cs")
         {
-            namespaces.Replace(temps[i], "");
-        }
-        scriptContent = scriptContent.Replace("#NAMESPACE#", namespaces);
+            // 这里实现自定义的一些规则
+            // scriptContent = scriptContent.Replace("#SCRIPTNAME#", Path.GetFileNameWithoutExtension(newFilePath));
+            var namespaces = Path.GetFileNameWithoutExtension(newFilePath);
+            for (int i = 0; i < temps.Length; i++)
+            {
+                namespaces.Replace(temps[i], "");
+            }
 
-        File.WriteAllText(realPath, scriptContent);
+            scriptContent = scriptContent.Replace("#NAMESPACE#", namespaces);
+
+            File.WriteAllText(realPath, scriptContent);
+        }
     }
 }
