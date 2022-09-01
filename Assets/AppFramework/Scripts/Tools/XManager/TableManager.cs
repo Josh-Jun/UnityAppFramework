3279618a7e972c06dd589492836b0ac32db3a77f
Josh <shijun_z@163.com>
2022-09-01 13:58:07 +0800

    update tablemanager

--- Assets/AppFramework/Scripts/Tools/XManager/TableManager.cs
+++ Assets/AppFramework/Scripts/Tools/XManager/TableManager.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Newtonsoft.Json;
+using LitJson;
 using ThriftTable;
 using UnityEngine;
 
@@ -12,6 +12,12 @@ public class TableManager : SingletonMono<TableManager>
     public override void InitManager(Transform parent)
     {
         base.InitManager(parent);
+        
+        RegiterType();
+        RegisterFloat();
+        RegisterVector3();
+        RegisterVector2();
+        RegisterQuaternion();
     }
     public void InitConfig()
     {
@@ -49,8 +55,7 @@ public class TableManager : SingletonMono<TableManager>
                 t = XmlManager.ProtoDeSerialize<T>(table.bytes);
                 break;
             case TableMold.Json:
-                t = JsonConvert.DeserializeObject<T>(table.text);
-                //t = LitJson.JsonMapper.ToObject<T>(table.text);
+                t = JsonMapper.ToObject<T>(table.text);
                 break;
             case TableMold.Thrift:
                 ThriftTableHolder.ETXT_NAME en = (ThriftTableHolder.ETXT_NAME)Enum.Parse(typeof(ThriftTableHolder.ETXT_NAME), tableName);
@@ -59,6 +64,91 @@ public class TableManager : SingletonMono<TableManager>
             
         }
         return t;
+    }
+    public static void RegiterType()
+    {
+        void Exporter(Type obj, JsonWriter writer)
+        {
+            writer.Write(obj.FullName);
+        }
+
+        JsonMapper.RegisterExporter((ExporterFunc<Type>)Exporter);
+
+        Type Importer(string obj)
+        {
+            return Type.GetType(obj);
+        }
+
+        JsonMapper.RegisterImporter((ImporterFunc<string, Type>)Importer);
+    }
+    private static void RegisterFloat()
+    {
+        void Exporter(float obj, JsonWriter writer)
+        {
+            writer.Write(obj);
+        }
+
+        JsonMapper.RegisterExporter((ExporterFunc<float>)Exporter);
+
+        float Importer(double obj)
+        {
+            return (float)obj;
+        }
+
+        JsonMapper.RegisterImporter((ImporterFunc<double, float>)Importer);
+    }
+    private static void RegisterVector3()
+    {
+        void Exporter(Vector3 obj, JsonWriter writer)
+        {
+            writer.WriteObjectStart();
+
+            writer.WritePropertyName("x");//写入属性名
+            writer.Write(obj.x);//写入值
+            writer.WritePropertyName("y");
+            writer.Write(obj.y);
+            writer.WritePropertyName("z");
+            writer.Write(obj.z);
+
+            writer.WriteObjectEnd();
+        }
+
+        JsonMapper.RegisterExporter((ExporterFunc<Vector3>)Exporter);//序列化
+    }
+
+    private static void RegisterVector2()
+    {
+        void Exporter(Vector2 obj, JsonWriter writer)
+        {
+            writer.WriteObjectStart();
+
+            writer.WritePropertyName("x");//写入属性名
+            writer.Write(obj.x);//写入值
+            writer.WritePropertyName("y");
+            writer.Write(obj.y);
+
+            writer.WriteObjectEnd();
+        }
+
+        JsonMapper.RegisterExporter((ExporterFunc<Vector2>)Exporter);//序列化
+    }
+
+    private static void RegisterQuaternion()
+    {
+        void Exporter(Quaternion obj, JsonWriter writer)
+        {
+            writer.WriteObjectStart();
+            writer.WritePropertyName("x");//写入属性名
+            writer.Write(obj.x);//写入值
+            writer.WritePropertyName("y");
+            writer.Write(obj.y);
+            writer.WritePropertyName("z");
+            writer.Write(obj.z);
+            writer.WritePropertyName("w");
+            writer.Write(obj.w);
+            writer.WriteObjectEnd();
+        }
+        JsonMapper.RegisterExporter((ExporterFunc<Quaternion>)Exporter);//序列化
     }
 }
 
