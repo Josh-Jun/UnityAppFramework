64e2db85fb85a24f2866b2d4f9fe112e102b726f
大腿Plus <shijun_z@163.com>
2022-09-18 11:48:06 +0800

    update uivideoplayer native size

--- Assets/AppFramework/Libraries/UIVideoPlayer/Scripts/UIVideoPlayer.cs
+++ Assets/AppFramework/Libraries/UIVideoPlayer/Scripts/UIVideoPlayer.cs
@@ -411,6 +411,9 @@ public class UIVideoPlayer : SingletonMono<UIVideoPlayer>
     {
         isReady = false;
         _audioVolume = VideoPlayer.GetDirectAudioVolume(0);
+        var rect = _videoDisplay.GetComponent<RectTransform>();
+        var vec = (transform as RectTransform).sizeDelta;
+        rect.sizeDelta = PictureManager.AdaptSize(vec, new Vector2(player.width, player.height));
     }
 
     // 准备完成事件调用
