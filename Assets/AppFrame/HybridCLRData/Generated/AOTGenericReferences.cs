b0ce3b78cafa963b8bc0fe76c9d466acd9b5b5ac
Josh@pc <shijun_z@163.com>
2023-02-15 11:42:06 +0800

    update ignore file

---
+++ Assets/AppFrame/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -0,0 +1,89 @@
+public class AOTGenericReferences : UnityEngine.MonoBehaviour
+{
+
+	// {{ constraint implement type
+	// }} 
+
+	// {{ AOT generic type
+	//DG.Tweening.Core.DOGetter`1<UnityEngine.Vector2>
+	//DG.Tweening.Core.DOGetter`1<System.Single>
+	//DG.Tweening.Core.DOGetter`1<UnityEngine.Vector3>
+	//DG.Tweening.Core.DOGetter`1<UnityEngine.Quaternion>
+	//DG.Tweening.Core.DOGetter`1<System.Object>
+	//DG.Tweening.Core.DOGetter`1<UnityEngine.Color>
+	//DG.Tweening.Core.DOSetter`1<System.Single>
+	//DG.Tweening.Core.DOSetter`1<UnityEngine.Vector3>
+	//DG.Tweening.Core.DOSetter`1<UnityEngine.Quaternion>
+	//DG.Tweening.Core.DOSetter`1<System.Object>
+	//DG.Tweening.Core.DOSetter`1<UnityEngine.Vector2>
+	//DG.Tweening.Core.DOSetter`1<UnityEngine.Color>
+	//DG.Tweening.Core.TweenerCore`3<UnityEngine.Quaternion,UnityEngine.Vector3,DG.Tweening.Plugins.Options.QuaternionOptions>
+	//DG.Tweening.Core.TweenerCore`3<UnityEngine.Vector3,System.Object,DG.Tweening.Plugins.Options.PathOptions>
+	//System.Action`1<System.Int32>
+	//System.Action`1<System.Byte>
+	//System.Action`1<AppFrame.Enum.UpdateMold>
+	//System.Action`1<System.Object>
+	//System.Action`1<System.Single>
+	//System.Action`2<System.Int64,System.Int64>
+	//System.Action`2<AppFrame.Enum.UpdateMold,System.Object>
+	//System.Action`2<System.Object,System.Int32>
+	//System.Action`2<System.Object,System.Object>
+	//System.Action`2<System.Object,System.Single>
+	//System.Action`3<System.Object,System.Object,System.Object>
+	//System.Action`4<DG.Tweening.Plugins.Options.PathOptions,System.Object,UnityEngine.Quaternion,System.Object>
+	//System.Collections.Generic.Dictionary`2<System.Object,System.Object>
+	//System.Collections.Generic.Dictionary`2/Enumerator<System.Object,System.Object>
+	//System.Collections.Generic.Dictionary`2/KeyCollection<System.Object,System.Object>
+	//System.Collections.Generic.Dictionary`2/KeyCollection/Enumerator<System.Object,System.Object>
+	//System.Collections.Generic.IEnumerator`1<System.Object>
+	//System.Collections.Generic.KeyValuePair`2<System.Object,System.Object>
+	//System.Collections.Generic.List`1<UnityEngine.Vector3>
+	//System.Collections.Generic.List`1<System.Int32>
+	//System.Collections.Generic.List`1<AppFrame.Tools.ExcelData>
+	//System.Collections.Generic.List`1<AppFrame.Tools.ExcelPackageData>
+	//System.Collections.Generic.List`1<System.Object>
+	//System.Collections.Generic.List`1<UnityEngine.EventSystems.RaycastResult>
+	//System.Collections.Generic.List`1/Enumerator<System.Object>
+	//System.Collections.Generic.Queue`1<System.Object>
+	//System.Func`1<System.Byte>
+	//System.Nullable`1<UnityEngine.Vector3>
+	//UnityEngine.Events.UnityAction`1<System.Object>
+	//UnityEngine.Events.UnityEvent`1<System.Object>
+	// }}
+
+	public void RefMethods()
+	{
+		// DG.Tweening.Core.TweenerCore`3<UnityEngine.Color,UnityEngine.Color,DG.Tweening.Plugins.Options.ColorOptions> DG.Tweening.Core.Extensions::Blendable<UnityEngine.Color,UnityEngine.Color,DG.Tweening.Plugins.Options.ColorOptions>(DG.Tweening.Core.TweenerCore`3<UnityEngine.Color,UnityEngine.Color,DG.Tweening.Plugins.Options.ColorOptions>)
+		// System.Object DG.Tweening.Core.Extensions::SetSpecialStartupMode<System.Object>(System.Object,DG.Tweening.Core.Enums.SpecialStartupMode)
+		// DG.Tweening.Core.TweenerCore`3<UnityEngine.Vector3,System.Object,DG.Tweening.Plugins.Options.PathOptions> DG.Tweening.DOTween::To<UnityEngine.Vector3,System.Object,DG.Tweening.Plugins.Options.PathOptions>(DG.Tweening.Plugins.Core.ABSTweenPlugin`3<UnityEngine.Vector3,System.Object,DG.Tweening.Plugins.Options.PathOptions>,DG.Tweening.Core.DOGetter`1<UnityEngine.Vector3>,DG.Tweening.Core.DOSetter`1<UnityEngine.Vector3>,System.Object,System.Single)
+		// System.Object DG.Tweening.TweenSettingsExtensions::OnStart<System.Object>(System.Object,DG.Tweening.TweenCallback)
+		// System.Object DG.Tweening.TweenSettingsExtensions::OnUpdate<System.Object>(System.Object,DG.Tweening.TweenCallback)
+		// System.Object DG.Tweening.TweenSettingsExtensions::SetEase<System.Object>(System.Object,DG.Tweening.Ease)
+		// System.Object DG.Tweening.TweenSettingsExtensions::SetLoops<System.Object>(System.Object,System.Int32,DG.Tweening.LoopType)
+		// System.Object DG.Tweening.TweenSettingsExtensions::SetRelative<System.Object>(System.Object)
+		// System.Object DG.Tweening.TweenSettingsExtensions::SetTarget<System.Object>(System.Object,System.Object)
+		// System.Object DG.Tweening.TweenSettingsExtensions::SetUpdate<System.Object>(System.Object,DG.Tweening.UpdateType)
+		// System.Object System.Activator::CreateInstance<System.Object>()
+		// System.Object[] System.Array::Empty<System.Object>()
+		// System.Void System.Array::Reverse<System.Byte>(System.Byte[])
+		// System.Object System.Linq.Enumerable::Last<System.Object>(System.Collections.Generic.IEnumerable`1<System.Object>)
+		// System.Collections.Generic.List`1<System.Object> System.Linq.Enumerable::ToList<System.Object>(System.Collections.Generic.IEnumerable`1<System.Object>)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter,Modules.Test.TestLogic/<LogText>d__6>(System.Runtime.CompilerServices.TaskAwaiter&,Modules.Test.TestLogic/<LogText>d__6&)
+		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder::Start<Modules.Test.TestLogic/<LogText>d__6>(Modules.Test.TestLogic/<LogText>d__6&)
+		// System.Object System.Threading.Interlocked::CompareExchange<System.Object>(System.Object&,System.Object,System.Object)
+		// System.Object UnityEngine.AndroidJavaObject::CallStatic<System.Object>(System.String,System.Object[])
+		// System.Int32 UnityEngine.AndroidJavaObject::CallStatic<System.Int32>(System.String,System.Object[])
+		// System.Object UnityEngine.AndroidJavaObject::GetStatic<System.Object>(System.String)
+		// System.Object UnityEngine.AssetBundle::LoadAsset<System.Object>(System.String)
+		// System.Object UnityEngine.Component::GetComponent<System.Object>()
+		// System.Object UnityEngine.Component::GetComponentInParent<System.Object>()
+		// System.Object[] UnityEngine.Component::GetComponentsInChildren<System.Object>()
+		// System.Boolean UnityEngine.EventSystems.ExecuteEvents::Execute<System.Object>(UnityEngine.GameObject,UnityEngine.EventSystems.BaseEventData,UnityEngine.EventSystems.ExecuteEvents/EventFunction`1<System.Object>)
+		// System.Object UnityEngine.GameObject::AddComponent<System.Object>()
+		// System.Object UnityEngine.GameObject::GetComponent<System.Object>()
+		// System.Object UnityEngine.JsonUtility::FromJson<System.Object>(System.String)
+		// System.Object UnityEngine.Object::FindObjectOfType<System.Object>()
+		// System.Object UnityEngine.Object::Instantiate<System.Object>(System.Object,UnityEngine.Transform)
+		// System.Object UnityEngine.Resources::Load<System.Object>(System.String)
+	}
+}
