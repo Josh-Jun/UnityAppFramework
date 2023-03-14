0e0277c9a122ca13fae7e667d6a4a769c98c70e7
Josh@pc <shijun_z@163.com>
2023-03-14 18:26:28 +0800

    update editor

---
+++ Assets/AppFrame/Editor/Tools/ToolkitsWindow/ToolkitsWindow.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class ToolkitsWindow : EditorWindow
+{
+    private VisualElement root;
+    private ListView leftListView;
+    private string[] itemsName =
+    {
+        "item1", 
+        "item2", 
+        "item3", 
+        "item4", 
+        "item5", 
+        "item6", 
+        "item7", 
+        "item8", 
+        "item9", 
+        "item10", 
+        "item11", 
+        "item12", 
+        "item13", 
+        "item14"
+    };
+
+
+    [MenuItem("Tools/ToolkitsWindow")]
+    public static void ShowExample()
+    {
+        ToolkitsWindow wnd = GetWindow<ToolkitsWindow>();
+        wnd.titleContent = new GUIContent("ToolkitsWindow");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        root = rootVisualElement;
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/AppFrame/Editor/Tools/ToolkitsWindow/ToolkitsWindow.uxml");
+        visualTree.CloneTree(root);
+
+        leftListView = root.Q<ListView>("left");
+        // leftListView.
+        leftListView.itemsSource = itemsName;
+        leftListView.makeItem = MakeListItem;
+        leftListView.bindItem = BindListItem;
+        leftListView.onSelectedIndicesChange += OnItemsChosen;
+    }
+
+    private void OnItemsChosen(IEnumerable<int> objs)
+    {
+        foreach (var index in objs)
+        {
+            Debug.Log(index);
+        }
+    }
+
+    private void BindListItem(VisualElement ve, int index)
+    {
+        var label = ve as Label;
+        label.text = itemsName[index];
+        label.name = index.ToString();
+    }
+
+    private VisualElement MakeListItem()
+    {
+        var label = new Label();
+        label.style.unityTextAlign = TextAnchor.MiddleLeft;
+        label.style.paddingLeft = 5;
+        return label;
+    }
+}
