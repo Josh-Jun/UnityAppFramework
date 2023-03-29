592edccf3b9ba750c8fa6d9872714f82f5136a1a
Josh@pc <shijun_z@163.com>
2023-03-29 14:58:08 +0800

    update editor

---
+++ Assets/AppFrame/Editor/Tools/ToolkitsWindow/BuildApp/BuildApp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PICO_XR_SETTING
+using UnityEditor.XR.Management;
+using UnityEditor.XR.Management.Metadata;
+using UnityEngine.XR.Management;
+#endif
+
+namespace AppFrame.Editor
+{
+    public class BuildApp
+    {
+        
+    }
+}
