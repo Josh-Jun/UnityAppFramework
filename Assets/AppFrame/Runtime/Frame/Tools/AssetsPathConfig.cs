a17626c6ddf4d18a08acfb2878dbb81b4f4e03e4
Josh@pc <shijun_z@163.com>
2023-03-31 12:16:50 +0800

    update editor

--- Assets/AppFrame/Runtime/Frame/Tools/AssetsPathConfig.cs
+++ Assets/AppFrame/Runtime/Frame/Tools/AssetsPathConfig.cs
@@ -4,17 +4,22 @@ namespace AppFrame.Data
     {
         public const string AppConfig = "Assets/Config/AppConfig";
         public const string AppScriptConfig = "Assets/Config/AppScriptConfig";
+        public const string mscorlib = "Assets/Dll/mscorlib";
+        public const string System = "Assets/Dll/System";
+        public const string UnityEngine = "Assets/Dll/UnityEngine";
         public const string AppScene = "Scenes/App/AppScene";
+        public const string update_progress = "Assets/Update/Textures/update_progress";
+        public const string UpdateView = "Assets/Update/Views/UpdateView";
         public const string TestScene = "Scenes/Test/TestScene";
         public const string UpdateScene = "Scenes/Update/UpdateScene";
         public const string AppTableConfig = "Table/Config/AppTableConfig";
         public const string TestJson = "Table/Json/TestJson";
+        public const string UserDataJson = "Table/Json/UserDataJson";
         public const string TestXml = "Table/Xml/TestXml";
+        public const string UserDataXml = "Table/Xml/UserDataXml";
         public const string ask_bg = "App/Ask/Textures/ask_bg";
         public const string AskView = "App/Ask/Views/AskView";
         public const string LoadingView = "App/Loading/Views/LoadingView";
-        public const string update_progress = "App/Update/Textures/update_progress";
-        public const string UpdateView = "App/Update/Views/UpdateView";
         public const string Cube = "Test/Common/Materials/Cube";
         public const string Quad = "Test/Common/Materials/Quad";
         public const string logo = "Test/Common/Textures/logo";
