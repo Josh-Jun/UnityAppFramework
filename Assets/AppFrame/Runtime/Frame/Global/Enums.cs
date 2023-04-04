9dd85bb515e3918382368c245742956a35df2cfb
Josh@pc <shijun_z@163.com>
2023-04-04 14:14:31 +0800

    update enum

--- Assets/AppFrame/Runtime/Frame/Global/Enums.cs
+++ Assets/AppFrame/Runtime/Frame/Global/Enums.cs
@@ -4,9 +4,9 @@ namespace AppFrame.Enum
     /// <summary>更新类型</summary>
     public enum UpdateMold
     {
-        None = 0,
-        Hotfix = 1,
-        App = 2,
+        None = 0,//不更新
+        Hotfix = 1,//更新资源
+        App = 2,//更新安装包
     }
 
     /// <summary>配置表类型</summary>
@@ -19,7 +19,7 @@ namespace AppFrame.Enum
     /// <summary>AB包类型</summary>
     public enum ABMold
     {
-        Hybrid = 0,
-        Assets = 1,
+        Hybrid = 0,//Dll资源AB
+        Assets = 1,//资源AB
     }
 }
