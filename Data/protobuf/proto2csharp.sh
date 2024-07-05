2b64f54fde3190701466a019fe850e424697d0c7
Josh@book <shijun_z@163.com>
2024-07-05 21:22:29 +0800

    update protobuf tool

--- Data/protobuf/proto2csharp.sh
+++ Data/protobuf/proto2csharp.sh
@@ -1,9 +1,9 @@
-base_dir=$(pwd)
+base_dir=$(dirname $0)
 filename="AppProtoData"
 # step1 generate
-./bin/protoc -I=${base_dir}/proto --csharp_out=${base_dir}/output ${base_dir}/proto/${filename}.proto
+${base_dir}/bin/protoc -I=${base_dir}/proto --csharp_out=${base_dir}/output ${base_dir}/proto/${filename}.proto
 # step2 copy
-base_target_dir=$(dirname $(dirname $(pwd)))
+base_target_dir=$(dirname $(dirname ${base_dir}))
 target_dir=${base_target_dir}/Assets/AppFrame/Runtime/Frame/Manager/Network/Data/
 cp ${base_dir}/output/${filename}.cs ${target_dir}
 cp ${base_dir}/proto/${filename}.proto ${target_dir}
