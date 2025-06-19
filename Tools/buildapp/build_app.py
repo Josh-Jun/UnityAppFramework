926257c3b8367836607ddcc1a48cd93fb9a670d0
Josh@win <shijun_z@163.com>
2025-06-19 18:20:20 +0800

    update buildapp py

--- Tools/buildapp/build_app.py
+++ Tools/buildapp/build_app.py
@@ -5,29 +5,24 @@ import time
 static_func = 'App.Editor.View.BuildApp.OneKeyBuild'
 # 调用unity中我们封装的静态函数
 def call_unity_static_func():
-    os.system('git reset HEAD^')
-    os.system('git pull')
 	log_file = sys.argv[2] + '/unity_build.log'
 	cmd = 'start %s -quit -batchmode -projectPath %s -logFile %s -executeMethod %s --version:%s --development:%s --assetplaymold:%s'%(sys.argv[1], sys.argv[2], log_file, static_func, sys.argv[3], sys.argv[4], sys.argv[5])
 	print('run cmd:  ' + cmd)
 	os.system(cmd)
-    os.system('git reset HEAD^')
 
 
 def monitor_unity_log(target_log):
 	pos = 0
 	path = sys.argv[2] + '/unity_build.log'
-	if os.path.isfile(path):
-		os.remove(path)
 	fp = None
 	print('Start Output Unity Log')
 	start_output = True
-	while start_output == True:
+	while start_output:
 		if os.path.isfile(path):
-			if fp == None:
+			if fp is None:
 				fp = open(path, 'r', encoding='utf-8', errors='ignore')
- 
-		if fp != None:
+
+		if fp is not None:
 			fp.seek(pos)
 			allLines = fp.readlines()
 			pos = fp.tell()
@@ -38,7 +33,9 @@ def monitor_unity_log(target_log):
 				if target_log in line:
 					start_output = False
 					break
-		time.sleep(0.5)
+			time.sleep(0.5)
+	os.system('cd ' + sys.argv[2])
+	os.system('git reset HEAD^')
 	print('End Output Unity Log')
 
 
