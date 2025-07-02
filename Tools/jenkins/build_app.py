4a6d36e345bd490d713c3d06569e4e31e56a9abc
Josh@win <shijun_z@163.com>
2025-07-02 11:38:11 +0800

    update tools buildapp.py

--- Tools/jenkins/build_app.py
+++ Tools/jenkins/build_app.py
@@ -5,18 +5,18 @@ import time
 static_func = 'App.Editor.View.BuildApp.OneKeyBuild'
 # 调用unity中我们封装的静态函数
 def call_unity_static_func():
-    print('Start Call Unity Static Func')
+    print('Call Unity Static Func Start')
     log_file = sys.argv[2] + '/unity_build.log'
     cmd = 'start %s -quit -batchmode -projectPath %s -logFile %s -executeMethod %s --version:%s --development:%s --assetplaymold:%s'%(sys.argv[1], sys.argv[2], log_file, static_func, sys.argv[3], sys.argv[4], sys.argv[5])
     print('run cmd:  ' + cmd)
     os.system(cmd)
-    print('End Call Unity Static Func')
+    print('Call Unity Static Func End')
 
 def monitor_unity_log(target_log):
     pos = 0
     path = sys.argv[2] + '/unity_build.log'
     fp = None
-    print('Start Output Unity Log')
+    print('Output Unity Log Start')
     start_output = True
     while start_output:
         if os.path.isfile(path):
@@ -35,21 +35,21 @@ def monitor_unity_log(target_log):
                     start_output = False
                     break
             time.sleep(0.5)
-    print('End Output Unity Log')
+    print('Output Unity Log End')
 
 
 def run_git_cmd():
-    print('Start Run Git Cmd')
+    print('Run Git Cmd Start')
     os.system('cd ' + sys.argv[2])
     os.system('git reset HEAD^')
     os.system('git checkout ' + sys.argv[6])
     os.system('git pull')
-    print('End Run Git Cmd')
+    print('Run Git Cmd End')
 
 
 if __name__ == '__main__':
-    print('Start BuildApp')
+    print('BuildApp Start')
     run_git_cmd()
     call_unity_static_func()
     monitor_unity_log('Build Complete!!!')
-    print('End BuildApp')
+    print('BuildApp End')
