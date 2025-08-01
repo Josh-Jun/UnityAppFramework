103e5f8b2d3f58e35288e16e12d68896431d8088
Josh@win <shijun_z@163.com>
2025-08-01 18:49:30 +0800

    update project setting

--- Assets/App/Scripts/Runtime/Helper/Asset/AssetPath.cs
+++ Assets/App/Scripts/Runtime/Helper/Asset/AssetPath.cs
@@ -13,6 +13,5 @@ public class AssetPath
     public const string MainScene = "Assets/Bundles/Hotfix/Scenes/MainScene.unity";
     public const string Snowflake = "Assets/Bundles/Hotfix/Videos/Snowflake.mp4";
     public const string DatePicker = "Assets/Bundles/Hotfix/Prefabs/UI/Items/DatePicker.prefab";
-    public const string PanelView = "Assets/Bundles/Hotfix/Prefabs/UI/Views/Panel/PanelView.prefab";
     public const string TestView = "Assets/Bundles/Hotfix/Prefabs/UI/Views/Test/TestView.prefab";
 }
