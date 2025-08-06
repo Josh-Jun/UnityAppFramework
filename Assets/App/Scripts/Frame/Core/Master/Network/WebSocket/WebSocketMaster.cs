d28f09ddeaf04577ffcd462722d83a592b7a127c
Josh@win <shijun_z@163.com>
2025-08-06 16:53:34 +0800

    add websocket

---
+++ Assets/App/Scripts/Frame/Core/Master/Network/WebSocket/WebSocketMaster.cs
@@ -0,0 +1,94 @@
+/* *
+ * ===============================================
+ * author      : Josh@win
+ * e-mail      : shijun_z@163.com
+ * create time : 2025年8月4 8:55
+ * function    : 
+ * ===============================================
+ * */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using App.Core.Tools;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace App.Core.Master
+{
+    public class WebSocketMaster : SingletonMonoEvent<WebSocketMaster>
+    {
+        private readonly ClientWebSocket _clientWebSocket;
+        private CancellationTokenSource _cancellationToken = new CancellationTokenSource();
+        private bool _isConnecting = false;
+        public Action<string> OnReceiveMessage { get; set; }
+
+        public WebSocketMaster()
+        {
+            _clientWebSocket = new ClientWebSocket();
+        }
+
+        public void Connect(string url, Action callback = null)
+        {
+            var uri = new Uri(url);
+            _clientWebSocket.ConnectAsync(uri, _cancellationToken.Token).ContinueWith(async t =>
+            {
+                callback?.Invoke();
+                _isConnecting = true;
+                TimeTaskMaster.Instance.AddTimeTask(() =>
+                {
+                    Send("ping");
+                }, 5, TimeUnit.Second, 0);
+                await Receive();
+            });
+        }
+        public void Close()
+        {
+            if (!_isConnecting) return;
+            _isConnecting = false;
+            _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+            _clientWebSocket.Dispose();
+            _cancellationToken.Cancel();
+        }
+
+        public void Send(string msg)
+        {
+            UniTask.Void(async () =>
+            {
+                var bytes = Encoding.UTF8.GetBytes(msg);
+                var array = new ArraySegment<byte>(bytes);
+                await _clientWebSocket.SendAsync(array, WebSocketMessageType.Text, true, _cancellationToken.Token);
+            });
+        }
+
+        public void Send(byte[] bytes)
+        {
+            UniTask.Void(async () =>
+            {
+                var array = new ArraySegment<byte>(bytes);
+                await _clientWebSocket.SendAsync(array, WebSocketMessageType.Binary, true, _cancellationToken.Token);
+            });
+        }
+
+        private async UniTask Receive()
+        {
+            while (true)
+            {
+                if (!_isConnecting) return;
+                var result = new byte[1024];
+                var message = new ArraySegment<byte>(result);
+                await _clientWebSocket.ReceiveAsync(message, new CancellationToken());
+                var msg = Encoding.UTF8.GetString(result);
+                await UniTask.SwitchToMainThread();
+                OnReceiveMessage?.Invoke(msg);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Close();
+        }
+    }
+}
