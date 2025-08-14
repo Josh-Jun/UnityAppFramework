eba51ee793e56d9fb76e9b559ad3476f1dfddd39
Josh@win <shijun_z@163.com>
2025-08-14 15:50:08 +0800

    move files

--- Assets/App/Scripts/Runtime/Helper/Asset/AssetPath.cs
+++ Assets/App/Scripts/Runtime/Helper/Asset/AssetPath.cs
@@ -6,15 +6,15 @@ public class AssetPath
     public const string LoadingScene = "Assets/Bundles/Builtin/Scenes/LoadingScene.unity";
     public const string UserJsonData = "Assets/Bundles/Builtin/Configs/Json/UserJsonData.json";
     public const string AdminXmlData = "Assets/Bundles/Builtin/Configs/Xml/AdminXmlData.xml";
-    public const string RedDot = "Assets/Bundles/Builtin/Prefabs/UI/Items/RedDot.prefab";
-    public const string Render3D2UIView = "Assets/Bundles/Builtin/Prefabs/Models/Views/Render3D2UI/Render3D2UIView.prefab";
     public const string ActionbarItem = "Assets/Bundles/Builtin/Prefabs/UI/Items/Ask/ActionbarItem.prefab";
     public const string DialogItem = "Assets/Bundles/Builtin/Prefabs/UI/Items/Ask/DialogItem.prefab";
     public const string SnackbarItem = "Assets/Bundles/Builtin/Prefabs/UI/Items/Ask/SnackbarItem.prefab";
     public const string ToastItem = "Assets/Bundles/Builtin/Prefabs/UI/Items/Ask/ToastItem.prefab";
     public const string AskView = "Assets/Bundles/Builtin/Prefabs/UI/Views/Ask/AskView.prefab";
     public const string LoadingView = "Assets/Bundles/Builtin/Prefabs/UI/Views/Loading/LoadingView.prefab";
-    public const string UpdateView = "Assets/Bundles/Builtin/Prefabs/UI/Views/Update/UpdateView.prefab";
     public const string MainScene = "Assets/Bundles/Hotfix/Scenes/MainScene.unity";
     public const string DatePicker = "Assets/Bundles/Hotfix/Prefabs/UI/Items/DatePicker.prefab";
+    public const string RedDot = "Assets/Bundles/Hotfix/Prefabs/UI/Items/RedDot.prefab";
+    public const string Render3D2UIView = "Assets/Bundles/Hotfix/Prefabs/Models/Views/Render3D2UI/Render3D2UIView.prefab";
+    public const string UpdateView = "Assets/Bundles/Hotfix/Prefabs/UI/Views/Update/UpdateView.prefab";
 }
