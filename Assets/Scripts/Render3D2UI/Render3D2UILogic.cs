b9079771db66f95eb5322dd818a01cadf5bc93ed
Josh@win <shijun_z@163.com>
2025-08-22 18:52:46 +0800

    update render3d2ui logic

--- Assets/Scripts/Render3D2UI/Render3D2UILogic.cs
+++ Assets/Scripts/Render3D2UI/Render3D2UILogic.cs
@@ -6,6 +6,7 @@
  * function    : 
  * ===============================================
  * */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using App.Core.Helper;
@@ -19,12 +20,23 @@ namespace App.Modules
 {
     public class RenderData
     {
-        public GameObject prefab;
+        public GameObject gameObject;
         public RawImage image;
+
+        public Vector3 position; // 模型位置
+        public Quaternion rotation; // 模型旋转
+
+        public Vector3 cameraOffset; // 相机偏移
+        public Vector3 cameraAngle; // 相机角度
+
+        public bool orthographic; // 相机是否是2D相机
+
+        public bool canScale; // 是否可以缩放
+        public bool isOffsetY; // 是否偏移Y轴
     }
 
     [LogicOf("Render3D2UI", AssetPath.Global)]
-    public class Render3D2UILogic : EventBase, ILogic
+    public class Render3D2UILogic : SingletonEvent<Render3D2UILogic>, ILogic
     {
         private Render3D2UIView View => ViewMaster.Instance.GetView<Render3D2UIView>();
 
@@ -34,7 +46,6 @@ namespace App.Modules
         {
             AddEventMsg<object>("OpenRender3D2UIView", OpenRender3D2UIView);
             AddEventMsg("CloseRender3D2UIView", CloseRender3D2UIView);
-
         }
 
         #region Life Cycle
@@ -65,26 +76,69 @@ namespace App.Modules
 
         #region Logic
 
+        private RenderData _renderData;
+
         private const float RotateSpeed = 40;
 
-        private void AddModel(GameObject prefab, RawImage image)
-        {
-            View.OpenView();
-            var model = Object.Instantiate(prefab, View.ModelTransform);
-            model.transform.localPosition = Vector3.zero;
-            model.transform.localRotation = Quaternion.identity;
-            model.transform.localScale = Vector3.one;
-            image.texture = RenderTexture;
-            EventListener.Get(image).onDrag = (go, delta) => { OnDragModel(model, delta); };
-        }
         private void OnDragModel(GameObject model, Vector2 delta)
         {
-            model.transform.Rotate(0, -delta.x * RotateSpeed * Time.deltaTime, 0);
+            var touchCount = 1;
+#if !UNITY_EDITOR
+            touchCount = Input.touchCount;
+#endif
+            // 单指旋转
+            if (touchCount == 1)
+            {
+                model.transform.Rotate(0, -delta.x * RotateSpeed * Time.deltaTime, 0);
+            }
+            // 双指缩放
+            if (touchCount == 2)
+            {
+                if (_renderData.canScale)
+                {
+                    var touchZero = Input.GetTouch(0);
+                    var touchOne = Input.GetTouch(1);
+                    var touchZeroPrev = touchZero.position - touchZero.deltaPosition;
+                    var touchOnePrev = touchOne.position - touchOne.deltaPosition;
+                    var prevMagnitude = (touchZeroPrev - touchOnePrev).magnitude;
+                    var currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+                    var difference = currentMagnitude - prevMagnitude;
+                    model.transform.localScale += Vector3.one * difference * 0.1f;
+                    model.transform.localScale = Vector3.one * Mathf.Clamp(model.transform.localScale.x, 1f, 3f);
+                    if (_renderData.isOffsetY)
+                    {
+                        model.transform.localPosition -= Vector3.up * difference * 0.15f;
+                        model.transform.localPosition = Vector3.up * Mathf.Clamp(model.transform.localPosition.y, -1003f, -1000f);
+                    }
+                }
+            }
         }
+
+#if UNITY_EDITOR
+        private void Update(float time)
+        {
+            if (_renderData.canScale)
+            {
+                var scale = Input.GetAxis("Mouse ScrollWheel");
+                View.ModelTransform.GetChild(0).localScale += Vector3.one * scale * 1f;
+                View.ModelTransform.GetChild(0).localScale = Vector3.one * Mathf.Clamp(View.ModelTransform.GetChild(0).localScale.x, 1f, 3f);
+
+                if (_renderData.isOffsetY)
+                {
+                    View.ModelTransform.GetChild(0).localPosition -= Vector3.up * scale * 1.5f;
+                    View.ModelTransform.GetChild(0).localPosition = Vector3.up * Mathf.Clamp(View.ModelTransform.GetChild(0).localPosition.y, -1003f, -1000f);
+                }
+            }
+        }
+#endif
 
         #endregion
 
         #region View Logic
+
+#if UNITY_EDITOR
+        private int _timeId;
+#endif
 
         private void OpenRender3D2UIView(object obj)
         {
@@ -92,6 +146,8 @@ namespace App.Modules
 
             if (obj is RenderData data)
             {
+                _renderData = data;
+                // 相机设置
                 var width = (int)data.image.rectTransform.rect.width;
                 var height = (int)data.image.rectTransform.rect.height;
                 RenderTexture = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGB32);
@@ -99,13 +155,24 @@ namespace App.Modules
                 View.RenderCamera.targetTexture = RenderTexture;
                 View.RenderCamera.clearFlags = CameraClearFlags.SolidColor;
                 View.RenderCamera.backgroundColor = Color.clear;
+                View.RenderCamera.orthographic = data.orthographic;
+                View.RenderCamera.transform.SetLocalPositionAndRotation(data.position + data.cameraOffset, Quaternion.Euler(data.cameraAngle));
                 View.RenderCamera.Render();
-                AddModel(data.prefab, data.image);
+                View.OpenView();
+                // 模型设置位置
+                data.gameObject.transform.SetParent(View.ModelTransform);
+                data.gameObject.transform.SetLocalPositionAndRotation(data.position, data.rotation);
+                data.image.texture = RenderTexture;
+                // 事件绑定
+                EventListener.Get(data.image).onDrag = (go, delta) => { OnDragModel(data.gameObject, delta); };
             }
             else
             {
                 Log.W("OpenRender3D2UIView obj is not RenderData");
             }
+#if UNITY_EDITOR
+            _timeId = TimeUpdateMaster.Instance.StartTimer(Update);
+#endif
         }
         private void CloseRender3D2UIView()
         {
@@ -115,8 +182,11 @@ namespace App.Modules
             RenderTexture.ReleaseTemporary(RenderTexture);
             foreach (Transform child in View.ModelTransform)
             {
-                Object.Destroy(child.gameObject);
+                UnityEngine.Object.Destroy(child.gameObject);
             }
+#if UNITY_EDITOR
+            TimeUpdateMaster.Instance.EndTimer(_timeId);
+#endif
         }
 
         #endregion
