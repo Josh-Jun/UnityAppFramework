dc4f9b0c2ea2299be2f667946e626f14d90c40d0
Josh@win <shijun_z@163.com>
2025-09-24 09:52:12 +0800

    update editor game view resolution

--- Assets/App/Scripts/Frame/Core/Master/View/ViewMaster.cs
+++ Assets/App/Scripts/Frame/Core/Master/View/ViewMaster.cs
@@ -243,7 +243,12 @@ namespace App.Core.Master
         /// <param name="orientation">0: Portrait, 1: LandscapeLeft, 2: LandscapeLeft(AutoRotation)</param>
         public async UniTask SwitchScreen(int orientation)
         {
-            ChangeGameViewResolution(orientation);
+#if UNITY_EDITOR
+            var type = AppHelper.GetAssemblyType("App.Editor.Helper.EditorHelper", "App.Editor");
+            var helper = Activator.CreateInstance(type);
+            var method = type.GetMethod("ChangeGameViewResolution");
+            method?.Invoke(helper, new object[] { orientation });
+#endif
             switch (orientation)
             {
                 case 0:
@@ -267,125 +272,6 @@ namespace App.Core.Master
 
             await UniTask.Delay(500);
             SafeAreaAdjuster();
-        }
-
-        private int Orientation;
-        private void ChangeGameViewResolution(int orientation)
-        {
-#if UNITY_EDITOR
-            Orientation = Screen.width < Screen.height ? 0 : 1;
-            var width = orientation == 0 ? 1170 : 2532;
-            var height = orientation == 0 ? 2532 : 1170;
-            SetGameViewSize(width, height);
-        }
-        
-        private void ClearGameViewCustomSize()
-        {
-            // 获取 GameViewSizes 单例实例
-            var gameViewSizesType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameViewSizes");
-            var scriptableSingletonType = typeof(UnityEditor.ScriptableSingleton<>).MakeGenericType(gameViewSizesType);
-            var instanceProp = scriptableSingletonType.GetProperty("instance");
-            var gameViewSizesInstance = instanceProp?.GetValue(null, null);
-
-            // 获取当前分组
-            var currentGroupProp = gameViewSizesInstance?.GetType().GetProperty("currentGroup");
-            var currentGroup = currentGroupProp?.GetValue(gameViewSizesInstance, null);
-
-            // 获取所有尺寸
-            var getTotalCount = currentGroup?.GetType().GetMethod("GetTotalCount");
-            var getBuiltinCount = currentGroup?.GetType().GetMethod("GetBuiltinCount");
-            var totalCount = (int)getTotalCount?.Invoke(currentGroup, null)!;
-            var builtinCount = (int)getBuiltinCount!.Invoke(currentGroup, null);
-
-            // 反向遍历并删除自定义尺寸
-            for (var i = totalCount - 1; i >= builtinCount; i--)
-            {
-                var getGameViewSize = currentGroup.GetType().GetMethod("GetGameViewSize");
-                var size = getGameViewSize?.Invoke(currentGroup, new object[] { i });
-
-                var sizeType = size?.GetType().GetProperty("sizeType");
-                var typeValue = (int)sizeType?.GetValue(size, null)!;
-
-                // 类型1是自定义分辨率
-                if (typeValue != 1) continue;
-                var removeCustomSize = currentGroup.GetType().GetMethod("RemoveCustomSize");
-                removeCustomSize?.Invoke(currentGroup, new object[] { i });
-            }
-        }
-
-        private void AddGameViewCustomSize(int width, int height, string displayName)
-        {
-            // 获取 GameViewSizes 单例实例
-            var gameViewSizesType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameViewSizes");
-            var scriptableSingletonType = typeof(UnityEditor.ScriptableSingleton<>).MakeGenericType(gameViewSizesType);
-            var instanceProp = scriptableSingletonType.GetProperty("instance");
-            var gameViewSizesInstance = instanceProp?.GetValue(null, null);
-
-            // 获取当前分组
-            var currentGroupProp = gameViewSizesInstance?.GetType().GetProperty("currentGroup");
-            var currentGroup = currentGroupProp?.GetValue(gameViewSizesInstance, null);
-
-            // 创建新的 GameViewSize
-            var gameViewSizeType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameViewSize");
-            var sizeTypeEnum = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameViewSizeType");
-            var ctor = gameViewSizeType.GetConstructor(
-                new Type[] { sizeTypeEnum, typeof(int), typeof(int), typeof(string) });
-
-            // 1 表示自定义分辨率类型
-            var newSize = ctor?.Invoke(new object[] { 1, width, height, displayName });
-
-            // 添加新尺寸
-            var addCustomSize = currentGroup?.GetType().GetMethod("AddCustomSize");
-            addCustomSize?.Invoke(currentGroup, new object[] { newSize });
-            UnityEditor.EditorUtility.RequestScriptReload();
-        }
-
-        private void SetGameViewSize(int width, int height)
-        {
-            var gameViewType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameView");
-            var gameView = UnityEditor.EditorWindow.GetWindow(gameViewType);
-
-            // 查找匹配的分辨率索引
-            var gameViewSizesInstance = typeof(UnityEditor.ScriptableSingleton<>)
-                .MakeGenericType(typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameViewSizes"))
-                .GetProperty("instance")!
-                .GetValue(null, null);
-
-            var currentGroup = gameViewSizesInstance.GetType()
-                .GetProperty("currentGroup")!
-                .GetValue(gameViewSizesInstance);
-
-            var getTotalCount = currentGroup.GetType().GetMethod("GetTotalCount");
-            var totalCount = (int)getTotalCount?.Invoke(currentGroup, null)!;
-            var index = -1;
-            for (var i = 0; i < totalCount; i++)
-            {
-                var getGameViewSize = currentGroup.GetType().GetMethod("GetGameViewSize");
-                var gameViewSize = getGameViewSize?.Invoke(currentGroup, new object[] { i });
-
-                var sizeWidth = (int)gameViewSize?.GetType().GetProperty("width")?.GetValue(gameViewSize)!;
-                var sizeHeight = (int)gameViewSize.GetType().GetProperty("height")?.GetValue(gameViewSize)!;
-
-                if (sizeWidth != width || sizeHeight != height) continue;
-                // 找到匹配的分辨率
-                index = i;
-                break;
-            }
-
-            if (index == -1)
-            {
-                var str = width > height ? "Landscape" : "Portrait";
-                AddGameViewCustomSize(width, height, $"{width}x{height} {str}");
-            }
-
-            var setSizeMethod = gameViewType.GetMethod("SizeSelectionCallback");
-            setSizeMethod?.Invoke(gameView, new object[] { index, null });
-#endif
-        }
-
-        private void OnDestroy()
-        {
-            ChangeGameViewResolution(Orientation);
         }
 
         public void InitBackgroundImage(Sprite sprite = null)
--- Assets/App/Scripts/Frame/Editor/Tools/SceneToolsEditor.cs
+++ Assets/App/Scripts/Frame/Editor/Tools/SceneToolsEditor.cs
@@ -1,3 +1,4 @@
+using App.Editor.Helper;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -9,25 +10,41 @@ namespace App.Editor.Tools
     public class SceneToolsEditor
     {
         private const int MENU_LEVEL = 0;
-        private const string MENU_PATH = "App/Editor/Launcher/AutoPLay _F8";
-        private const string MENU_VALUE = "MENU_VALUE";
+        private const string MENU_AUTO_PLAY_PATH = "App/Editor/Launcher/AutoPLay _F8";
+        private const string MENU_AUTO_PLAY_VALUE = "MENU_AUTO_PLAY_VALUE";
         private const string TEMP_SCENE = "TEMP_SCENE";
+        
+        
+        private const string MENU_RESTORE_GAME_VIEW_PATH = "App/Editor/Launcher/RestoreGameView _F11";
+        private const string MENU_RESTORE_GAME_VIEW_VALUE = "MENU_RESTORE_GAME_VIEW_VALUE";
         static SceneToolsEditor()
         {
             EditorApplication.playModeStateChanged += EditorApplication_PlayModeStateChanged;
-            if (!PlayerPrefs.HasKey(MENU_VALUE))
+            if (!PlayerPrefs.HasKey(MENU_AUTO_PLAY_VALUE))
             {
-                PlayerPrefs.SetInt(MENU_VALUE, 0);
+                PlayerPrefs.SetInt(MENU_AUTO_PLAY_VALUE, 0);
             }
-            Menu.SetChecked(MENU_PATH, PlayerPrefs.GetInt(MENU_VALUE) == 1);
+            Menu.SetChecked(MENU_AUTO_PLAY_PATH, PlayerPrefs.GetInt(MENU_AUTO_PLAY_VALUE) == 1);
+            if (!PlayerPrefs.HasKey(MENU_RESTORE_GAME_VIEW_VALUE))
+            {
+                PlayerPrefs.SetInt(MENU_RESTORE_GAME_VIEW_VALUE, 1);
+            }
+            Menu.SetChecked(MENU_RESTORE_GAME_VIEW_PATH, PlayerPrefs.GetInt(MENU_RESTORE_GAME_VIEW_VALUE) == 1);
         }
         
-        [MenuItem(MENU_PATH, false, MENU_LEVEL)]
+        [MenuItem(MENU_AUTO_PLAY_PATH, false, MENU_LEVEL)]
         public static void AutoOpenScene()
         {
-            Menu.SetChecked(MENU_PATH, PlayerPrefs.GetInt(MENU_VALUE) != 1);
-            var value = Menu.GetChecked(MENU_PATH) ? 1 : 0;
-            PlayerPrefs.SetInt(MENU_VALUE, value);
+            Menu.SetChecked(MENU_AUTO_PLAY_PATH, PlayerPrefs.GetInt(MENU_AUTO_PLAY_VALUE) != 1);
+            var value = Menu.GetChecked(MENU_AUTO_PLAY_PATH) ? 1 : 0;
+            PlayerPrefs.SetInt(MENU_AUTO_PLAY_VALUE, value);
+        }
+        [MenuItem(MENU_RESTORE_GAME_VIEW_PATH, false, MENU_LEVEL)]
+        public static void RestoreGameView()
+        {
+            Menu.SetChecked(MENU_RESTORE_GAME_VIEW_PATH, PlayerPrefs.GetInt(MENU_RESTORE_GAME_VIEW_VALUE) != 1);
+            var value = Menu.GetChecked(MENU_RESTORE_GAME_VIEW_PATH) ? 1 : 0;
+            PlayerPrefs.SetInt(MENU_RESTORE_GAME_VIEW_VALUE, value);
         }
 
         private static void OpenScene(int index = 0)
@@ -48,7 +65,7 @@ namespace App.Editor.Tools
             {
                 case PlayModeStateChange.EnteredEditMode: //停止播放事件监听后被监听
                     // Debug.Log("如果编辑器应用程序处于编辑模式而之前处于播放模式，则在编辑器应用程序的下一次更新期间发生。");
-                    if (Menu.GetChecked(MENU_PATH))
+                    if (Menu.GetChecked(MENU_AUTO_PLAY_PATH))
                     {
                         if (PlayerPrefs.HasKey(TEMP_SCENE))
                         {
@@ -56,10 +73,15 @@ namespace App.Editor.Tools
                             PlayerPrefs.DeleteKey(TEMP_SCENE);
                         }
                     }
+
+                    if (Menu.GetChecked(MENU_RESTORE_GAME_VIEW_PATH))
+                    {
+                        EditorHelper.RestoreGameViewResolution();
+                    }
                     break;
                 case PlayModeStateChange.ExitingEditMode: //编辑转播放时监听(播放之前)
                     // Debug.Log("在退出编辑模式时，在编辑器处于播放模式之前发生。");
-                    if (Menu.GetChecked(MENU_PATH))
+                    if (Menu.GetChecked(MENU_AUTO_PLAY_PATH))
                     {
                         if (SceneManager.GetActiveScene().path != string.Empty)
                         {
