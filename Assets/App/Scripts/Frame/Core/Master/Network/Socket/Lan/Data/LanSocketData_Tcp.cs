5bcf71800da2e9665d975fec2d10a2672ea09b52
Josh@win <shijun_z@163.com>
2026-01-07 18:02:13 +0800

    fix：新增socket通信数据类

---
+++ Assets/App/Scripts/Frame/Core/Master/Network/Socket/Lan/Data/LanSocketData_Tcp.cs
@@ -0,0 +1,19 @@
+/* *
+ * ===============================================
+ * author      : Josh@win
+ * e-mail      : shijun_z@163.com
+ * create time : 2026年1月7 17:4
+ * function    :
+ * ===============================================
+ * */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using App.Core.Helper;
+using UnityEngine;
+
+namespace App.Core.Master
+{
+    
+}
