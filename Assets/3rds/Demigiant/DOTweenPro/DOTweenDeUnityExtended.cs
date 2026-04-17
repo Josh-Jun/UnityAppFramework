c2353c951606c41cf9425147d8b747032e5a0e1d
Josh@win <shijun_z@163.com>
2026-04-17 20:18:27 +0800

    fix：更新框架核心包[App.Core]
    移出DOTween插件

---
+++ Assets/3rds/Demigiant/DOTweenPro/DOTweenDeUnityExtended.cs
@@ -0,0 +1,17 @@
+/* *
+ * ===============================================
+ * author      : Josh@win
+ * e-mail      : shijun_z@163.com
+ * create time : 2026年4月17 20:13
+ * function    : 
+ * ===============================================
+ * */
+// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2015/03/27 19:02
+// 
+// License Copyright (c) Daniele Giardini.
+// This work is subject to the terms at http://dotween.demigiant.com/license.php
+
+
+#if false // MODULE_MARKER
+#endif
